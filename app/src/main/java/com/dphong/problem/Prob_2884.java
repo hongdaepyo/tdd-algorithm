2181f198168a472379c928d43b59b327d4fbd65a
Hong Daepyo <h1o1n1g1@nate.com>
2021-07-05 21:58:01 +0900

    feat: prob.2884

---
+++ app/src/main/java/com/dphong/problem/Prob_2884.java
@@ -0,0 +1,35 @@
+package com.dphong.problem;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Prob_2884 {
+    private static StringTokenizer st;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String timeString = br.readLine();
+        System.out.println(solve(timeString));
+    }
+
+    public static String solve(String timeString) {
+        st = new StringTokenizer(timeString);
+        int hour = Integer.parseInt(st.nextToken());
+        int minute = Integer.parseInt(st.nextToken());
+
+        int minutes = timeToMinutes(hour, minute);
+
+        if (minutes >= 45) {
+            minutes -= 45;
+        } else {
+            minutes = 24 * 60 + minutes - 45;
+        }
+        return String.join(" ", String.valueOf(minutes / 60), String.valueOf(minutes % 60));
+    }
+
+    private static int timeToMinutes(int hour, int minute) {
+        return hour * 60 + minute;
+    }
+}
---
+++ app/src/test/java/com/dphong/problem/Prob_2884Test.java
@@ -0,0 +1,20 @@
+package com.dphong.problem;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Prob_2884Test {
+
+    Prob_2884 prob = new Prob_2884();
+
+    @Test
+    void solve() {
+        assertEquals("9 25", prob.solve("10 10"));
+        assertEquals("23 45", prob.solve("0 30"));
+        assertEquals("22 55", prob.solve("23 40"));
+        assertEquals("23 15", prob.solve("0 0"));
+        assertEquals("23 14", prob.solve("23 59"));
+        assertEquals("0 0", prob.solve("0 45"));
+    }
+}
