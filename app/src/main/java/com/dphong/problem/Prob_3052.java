763d9266540266d122c7f5b8e3e1df5558800073
Hong daepyo <h1o1n1g1@nate.com>
2021-07-14 01:17:38 +0900

    feat: Prob.3052

---
+++ app/src/main/java/com/dphong/problem/Prob_3052.java
@@ -0,0 +1,28 @@
+package com.dphong.problem;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Prob_3052 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int[] arr = new int[43];
+
+        int idx = 10;
+        int count = 0;
+        while (idx-- > 0) {
+            int num = Integer.parseInt(br.readLine());
+            int remain = num % 42;
+
+            if (arr[remain] == 0) {
+                count++;
+            }
+
+            arr[remain]++;
+        }
+
+        System.out.println(count);
+    }
+}
