ff2f7347663892721f7a7b6e0227f9ca5e6faee8
Hong daepyo <h1o1n1g1@nate.com>
2021-07-16 00:39:36 +0900

    feat: prob.4344

---
+++ app/src/main/java/com/dphong/problem/Prob_4344.java
@@ -0,0 +1,37 @@
+package com.dphong.problem;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Prob_4344 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int c = Integer.parseInt(br.readLine());
+
+        StringTokenizer st;
+        while (c-- > 0) {
+            st = new StringTokenizer(br.readLine());
+
+            int sum = 0;
+            int num = Integer.parseInt(st.nextToken());
+            int[] arr = new int[num];
+
+            for (int i = 0; i < num; i++) {
+                int n = Integer.parseInt(st.nextToken());
+                sum += n;
+                arr[i] = n;
+            }
+
+            float avg = (float) sum / num;
+
+            int over = (int) Arrays.stream(arr).filter(n -> n > avg).count();
+
+            System.out.println(String.format("%.3f%%", (float) over / num * 1000 / 1000 * 100));
+        }
+
+    }
+}
