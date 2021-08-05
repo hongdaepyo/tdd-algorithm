8a7d6c335a2d78d6ec24a6c475f7244f26f5481b
Hong Daepyo <h1o1n1g1@nate.com>
2021-08-06 01:15:02 +0900

    refactor: step.4-2 test solve

--- app/src/test/java/com/dphong/problem/Prob_10807Test.java
+++ app/src/test/java/com/dphong/problem/Prob_10807Test.java
@@ -25,15 +25,19 @@ public class Prob_10807Test {
 
     @Test
     void solve() {
-
-        assertEquals(0, prob.solve(0));
-        assertEquals(1, prob.solve(1));
-        assertEquals(1, prob.solve(2));
-        assertEquals(2, prob.solve(3));
-        assertEquals(3, prob.solve(4));
-        assertEquals(5, prob.solve(5));
-        assertEquals(8, prob.solve(6));
-        assertEquals(13, prob.solve(7));
-        assertEquals(21, prob.solve(8));
+        int[] arr = new int[]{0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597};
+        for (int i = 0; i < arr.length; i++) {
+            assertEquals(arr[i], prob.solve(i));
+        }
+//
+//        assertEquals(0, prob.solve(0));
+//        assertEquals(1, prob.solve(1));
+//        assertEquals(1, prob.solve(2));
+//        assertEquals(2, prob.solve(3));
+//        assertEquals(3, prob.solve(4));
+//        assertEquals(5, prob.solve(5));
+//        assertEquals(8, prob.solve(6));
+//        assertEquals(13, prob.solve(7));
+//        assertEquals(21, prob.solve(8));
     }
 }
